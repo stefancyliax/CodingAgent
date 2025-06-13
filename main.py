93fc77058363416145c6e6f5fba4986037403c51
stefancyliax <stefan.cyliax@gmail.com>
2025-06-13 14:53:12 +0200

    Implemented feedback loop to create an agentic system

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     VERBOSE = False 
+    MAX_CALLS = 20
     system_prompt = """
     You are a helpful AI coding agent.
 
@@ -45,44 +46,62 @@ def main():
     except Exception as e:
         print(f"Error initializing Gemini client: {e}")
         sys.exit(1)
-
+    
     # Call LLM    
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
-    try:
-        response = client.models.generate_content(
-            model='gemini-2.0-flash-001',
-            contents=messages,
-            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-            )
-    except Exception as e:
-        print(f"Error generating content: {e}")
-        sys.exit(1)
+    calls = 0
+    while True:
+        calls += 1
+        if calls > MAX_CALLS:
+            print(f"Maximum number of calls ({MAX_CALLS}) reached.")
+            sys.exit(1)
+                
 
-    # Process response
-    if VERBOSE:
-        print(f"User prompt: {user_prompt}")
-        if response.usage_metadata:
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        try:
+            response = client.models.generate_content(
+                model='gemini-2.0-flash-001',
+                contents=messages,
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+                )
+        except Exception as e:
+            print(f"Error generating content: {e}")
+            sys.exit(1)
 
-    if not response.function_calls:
-        return response.text
-
-    function_responses = []
-    for function_call_part in response.function_calls:
-        # print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-        function_call_result = call_function(function_call_part, VERBOSE)
-
-        if not function_call_result.parts[0].function_response:
-            raise Exception(f"Error: Function response empty")
-
-        function_responses.append(function_call_result.parts[0])
+        # Process response
         if VERBOSE:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+            print(f"User prompt: {user_prompt}")
+            if response.usage_metadata:
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-    if not function_responses:
-        raise Exception("no function responses generated, exiting.")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+
+        if not response.function_calls:
+            print(response.text)
+            return
+        else: 
+            function_responses = []
+            for function_call_part in response.function_calls:
+                function_call_result = call_function(function_call_part, VERBOSE)
+
+                if not function_call_result.parts[0].function_response:
+                    raise Exception(f"Error: Function response empty")
+
+                function_responses.append(function_call_result.parts[0])
+                if VERBOSE:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            if not function_responses:
+                raise Exception("no function responses generated, exiting.")
+        
+            messages.append(types.Content(role="tool", parts=function_responses))
+
+            
+
 
 
 
